e4a79e0047f86a3664eb8ed11b6ab5753637f683
SokolMks <sokolenkomks@gmail.com>
2021-10-06 15:18:17 +0300

    Added redux and stuff

---
+++ src/redux/contacts/contacts-selectors.js
@@ -0,0 +1,21 @@
+import { createSelector } from '@reduxjs/toolkit';
+
+const getFilter = state => state.contacts.filter;
+
+const getAllContacts = state => state.contacts.items;
+
+const getFilteredContacts = createSelector(
+  [getAllContacts, getFilter],
+  (contacts, filter) => {
+    const normalizeFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeFilter),
+    );
+  },
+);
+
+export default {
+  getFilter,
+  getFilteredContacts,
+};
